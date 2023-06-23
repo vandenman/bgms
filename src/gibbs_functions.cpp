35ebee515bfef19e02c6c003314c32916aebcc25
Don van den Bergh <donvdbergh@hotmail.com>
2023-06-23 15:47:58 +0200

    use paralel argument

--- src/gibbs_functions.cpp
+++ src/gibbs_functions.cpp
@@ -50,7 +50,7 @@ double compute_c(const int no_persons,
                  NumericVector g, NumericVector q,
                  const bool parallel)
 {
-  if (true)
+  if (parallel)
     return compute_c_parallel(no_persons,
                               node,
                               category,
